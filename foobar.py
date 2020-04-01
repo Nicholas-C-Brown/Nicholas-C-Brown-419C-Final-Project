324c310851509f6548bcc0b6aa51764a4a8b1c94
Nicholas-C-Brown <nicholas.c.brown@alumni.ubc.ca>
2020-04-01 12:17:56 -0700

    stuff

--- foobar.py
+++ foobar.py
@@ -1,5 +1,7 @@
 
 import requests
+import webbrowser
+import socket
 
 URL = "https://www.linkedin.com/oauth/v2/authorization"
 
@@ -8,7 +10,7 @@ RESPONSE_TYPE = r"code"
 CLIENT_ID = r"867nxtorfvyz78"
 CLIENT_SECRET = r"eSn4kF8wysqNA6XF"
 
-REDIRECT_URI = r"http://localhost:8000"
+REDIRECT_URI = r"http://192.168.56.1:8000"
 
 SCOPE = r"r_liteprofile"
 
@@ -16,7 +18,23 @@ PARAMS = {"response_type":RESPONSE_TYPE,"client_id":CLIENT_ID,"redirect_uri":RED
 
 request = requests.get(url = URL, params=PARAMS)
 
-print(request.text)
+webbrowser.open(request.url)
+
+#create an INET, STREAMing socket
+serversocket = socket.socket(
+    socket.AF_INET, socket.SOCK_STREAM)
+#bind the socket to a public host,
+# and a well-known port
+serversocket.bind((socket.gethostname(), 8000))
+#become a server socket
+
+serversocket.listen(1)
+
+while 1:
+    #accept connections from outside
+    (clientsocket, address) = serversocket.accept()
+
+    print("CLIENT:",clientsocket)
 
 
 
