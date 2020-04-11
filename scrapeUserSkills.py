6d4c311d5829b38943968f44ff9ce44f45129119
Nate <natewick005@gmail.com>
2020-04-11 16:30:21 -0700

    works omfg
    omfg

--- scrapeUserSkills.py
+++ scrapeUserSkills.py
@@ -6,8 +6,9 @@ import time
 import csv
 import json
 
-# CODE ADAPTED FROM https://www.linkedin.com/pulse/how-easy-scraping-data-from-linkedin-profiles-david-craven/
 
+# CODE ADAPTED FROM https://www.linkedin.com/pulse/how-easy-scraping-data-from-linkedin-profiles-david-craven/
+count = 0
 username = "cardiacexorcist@gmail.com"
 password = "419CEpicStyle"
 
@@ -19,9 +20,9 @@ usernamefield = driver.find_element_by_id("username")
 passwordfield = driver.find_element_by_id("password")
 
 passwordfield.send_keys(password)
-time.sleep(0.2)
+time.sleep(0.1)
 usernamefield.send_keys(username)
-time.sleep(0.2)
+time.sleep(0.1)
 
 log_in_button = driver.find_element_by_xpath("//*[@type='submit']")
 log_in_button.click()
@@ -29,38 +30,57 @@ time.sleep(.2)
 #
 
 # Open saved urls
-# linkedIn = []
-# urls = []
-# with open("data/urls.json", "r") as fin:
-#     data = fin.readline()
-#     linkedIn = json.loads(data)
-#     for url in linkedIn:
-#         urls.append("https://" + url.strip("'"))
+linkedIn = []
+urls = []
+with open("data/urls.json", "r") as fin:
+    data = fin.readline()
+    linkedIn = json.loads(data)
+    for url in linkedIn:
+        urls.append("https://" + url.strip("'"))
+
+print(urls)
 #
-# print(urls)
-#
-testurl = "https://www.linkedin.com/in/nicholas-c-brown/"
+#testurl = "https://www.linkedin.com/in/nicholas-c-brown/"
+for testurl in urls:
+    driver.get(testurl)
+    scroll = 250
+    while True:
+        driver.execute_script("window.scrollTo(0, " + str(scroll) + ");")
+        try:
+            button = driver.find_element_by_xpath("//button[@class='pv-profile-section__card-action-bar pv-skills-section__additional-skills artdeco-container-card-action-bar artdeco-button artdeco-button--tertiary artdeco-button--3 artdeco-button--fluid']")
+            button.click()
+            time.sleep(.5)
+            all_spans = driver.find_elements_by_xpath('//span[@class="pv-skill-category-entity__name-text t-16 t-black t-bold"]')
+            break
+        except:
+            time.sleep(0.3)
+            scroll+=250
 
-driver.get(testurl)
-time.sleep(3)
-sel = Selector(text=driver.page_source)
+
+    skills = []
+
+    for span in all_spans:
+        print(span.text)
+        skills.append(str(span.text))
+
+    for skill in skills:
+        print(skill)
 
 
-button = driver.find_element_by_xpath("//button[@class='pv-profile-section__card-action-bar pv-skills-section__additional-skills artdeco-container-card-action-bar artdeco-button artdeco-button--tertiary artdeco-button--3 artdeco-button--fluid']")
-button.click()
+    with open("data/userSkills.json", "r") as fin:
+        skilldict = json.load(fin)
 
-all_spans = driver.find_elements_by_xpath('//span[@class="pv-skill-category-entity__name-text t-16 t-black t-bold"]')
-# all_spans = driver.find_elements_by_css_selector('span.class.pv-skill-category-entity__name-text')
-
-print(all_spans)
-
-for span in all_spans:
-    print(span.get_attribute("innerHTML"))
-
-skills = []
-
+    with open("data/userSkills.json", "w") as fout:
+        skilldict[str(count)] = skills
+        count+=1
+        skillsout = json.dumps(skilldict)
+        fout.write(skillsout)
 
 driver.close()
+
+
+
+
 
 
 
