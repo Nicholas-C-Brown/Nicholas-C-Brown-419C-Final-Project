d6b1a1585f9122cc5fa5bce42081a5eda01a667a
Nate <natewick005@gmail.com>
2020-04-11 15:51:31 -0700

    zzz
    zzz

---
+++ scrapeUserSkills.py
@@ -0,0 +1,73 @@
+import ipython_genutils
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from parsel import Selector
+import time
+import csv
+import json
+
+# CODE ADAPTED FROM https://www.linkedin.com/pulse/how-easy-scraping-data-from-linkedin-profiles-david-craven/
+
+username = "cardiacexorcist@gmail.com"
+password = "419CEpicStyle"
+
+#  LOG IN TO LINKED IN
+driver = webdriver.Chrome("/Users/Nate/Desktop/cosc419/project/chromedriver")
+driver.get('https://linkedin.com/login')
+
+usernamefield = driver.find_element_by_id("username")
+passwordfield = driver.find_element_by_id("password")
+
+passwordfield.send_keys(password)
+time.sleep(0.2)
+usernamefield.send_keys(username)
+time.sleep(0.2)
+
+log_in_button = driver.find_element_by_xpath("//*[@type='submit']")
+log_in_button.click()
+time.sleep(.2)
+#
+
+# Open saved urls
+# linkedIn = []
+# urls = []
+# with open("data/urls.json", "r") as fin:
+#     data = fin.readline()
+#     linkedIn = json.loads(data)
+#     for url in linkedIn:
+#         urls.append("https://" + url.strip("'"))
+#
+# print(urls)
+#
+testurl = "https://www.linkedin.com/in/nicholas-c-brown/"
+
+driver.get(testurl)
+time.sleep(3)
+sel = Selector(text=driver.page_source)
+
+
+button = driver.find_element_by_xpath("//button[@class='pv-profile-section__card-action-bar pv-skills-section__additional-skills artdeco-container-card-action-bar artdeco-button artdeco-button--tertiary artdeco-button--3 artdeco-button--fluid']")
+button.click()
+
+all_spans = driver.find_elements_by_xpath('//span[@class="pv-skill-category-entity__name-text t-16 t-black t-bold"]')
+# all_spans = driver.find_elements_by_css_selector('span.class.pv-skill-category-entity__name-text')
+
+print(all_spans)
+
+for span in all_spans:
+    print(span.get_attribute("innerHTML"))
+
+skills = []
+
+
+driver.close()
+
+
+
+
+
+
+
+
+
+
